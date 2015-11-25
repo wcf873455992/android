60b5023a77f40f20674592b1633f19902d141a31
wcf873455992 <wcf873455992@sina.com>
2015-11-25 13:27:04 +0800

    record play v1.0.0

--- record_play/src/com/record_play/MainActivity.java
+++ record_play/src/com/record_play/MainActivity.java
@@ -2,8 +2,8 @@ package com.record_play;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+//import android.view.Menu;
+//import android.view.MenuItem;
 
 import java.io.IOException;  
 
